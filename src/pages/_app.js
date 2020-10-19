8a1b04c4b2c12725c90817ba115e8718bd4a6722
Andrew Miracle <me@andrewmiracle.com>
2020-10-19 16:44:06 +0000

    update meta tags in app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,47 +13,47 @@ export default function MyApp({ Component, pageProps }) {
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
         <link rel="stylesheet" href="/plyr.css" />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
+        <meta name="description" content="Follow the #EndSARS Protest live on Soro Soke Radio" />
+        <meta name="keywords" content="#Soro Soke, #EndSARS Online Radio, SoroSoke Nigeria, #EndPoliceBrutality" />
         <title>Soro Soke Radio</title>
 
-        <meta name='application-name' content='Soro Soke Online' />
+        <meta name='application-name' content='#SoroSoke Radio' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <meta name='apple-mobile-web-app-title' content='PWA App' />
-        <meta name='description' content='Best PWA App in the world' />
+        <meta name='apple-mobile-web-app-title' content='#SoroSoke Radio' />
+        <meta name='description' content='Follow the #EndSARS Protest live on Soro Soke Radio' />
+        <meta name='mobile-web-app-capable' content='yes' />
+
         {/*
         <meta name='format-detection' content='telephone=no' />
-        <meta name='mobile-web-app-capable' content='yes' />
         <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
-        <meta name='msapplication-TileColor' content='#2B5797' />
         <meta name='msapplication-tap-highlight' content='no' />
-        <meta name='theme-color' content='#000000' />
 
-        <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
-        <link rel='manifest' href='/static/manifest.json' />
         <link rel='mask-icon' href='/static/icons/safari-pinned-tab.svg' color='#5bbad5' />
-        <link rel='shortcut icon' href='/static/icons/favicon.ico' />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+        
+        
+      */}
 
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:url' content='https://yourdomain.com' />
-        <meta name='twitter:title' content='PWA App' />
-        <meta name='twitter:description' content='Best PWA App in the world' />
-        <meta name='twitter:image' content='https://yourdomain.com/static/icons/android-chrome-192x192.png' />
-        <meta name='twitter:creator' content='@DavidWShadow' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <meta name='twitter:card' content='Soro Soke Online Radio' />
+        <meta name='twitter:url' content='https://soro-soke-radio.vercel.app' />
+        <meta name='twitter:title' content='#SoroSoke Radio' />
+        <meta name='twitter:description' content='Soro Soke Online Radio' />
+        <meta name='twitter:image' content='https://soro-soke-radio.vercel.app/logo.png' />
+        <meta name='twitter:creator' content='@feyikewa' />
+
         <meta property='og:type' content='website' />
-        <meta property='og:title' content='PWA App' />
-        <meta property='og:description' content='Best PWA App in the world' />
-        <meta property='og:site_name' content='PWA App' />
-        <meta property='og:url' content='https://yourdomain.com' />
-        <meta property='og:image' content='https://yourdomain.com/static/icons/apple-touch-icon.png' />
-        */}
+        <meta property='og:title' content='#SoroSoke Radio' />
+        <meta property='og:description' content='Soro Soke Online Radio' />
+        <meta property='og:site_name' content='#SoroSoke Radio' />
+        <meta property='og:url' content='https://soro-soke-radio.vercel.app' />
+        <meta property='og:image' content='https://soro-soke-radio.vercel.app/logo.png' />
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/favicon-16x16.png"
+          href="/favicon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
@@ -64,8 +64,10 @@ export default function MyApp({ Component, pageProps }) {
           type="image/png"
           sizes="32x32"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <link rel="apple-touch-icon" href="/logo.png"></link>
+        <meta name="theme-color" content="#fadc58" />
+        <meta name='msapplication-TileColor' content='#fadc58' />
+
       </Head>
       <Component {...pageProps} />
     </>
